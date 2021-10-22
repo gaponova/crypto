93965376cc1b8ecb339fed7a821bf2ab8877afe4
gaponova <dzensmart@gmail.com>
2021-10-22 16:10:14 +0300

    Сделали header

--- js/menu-burger.js
+++ js/menu-burger.js
@@ -2,6 +2,6 @@
 $(document).ready(function(){
   $('.header__burger').click(function(event){
     $('.header__burger, .header__menu').toggleClass('active');
-    // $('body').toggleClass('lock');
+    $('body').toggleClass('lock');
   });
 });
