96e7ef8cf0b8c7b86e9110e698c1e837141a4ff8
gaponova <dzensmart@gmail.com>
2021-11-11 22:06:57 +0200

    Сделали спойлеры с адаптивом

--- js/spoilers.js
+++ js/spoilers.js
@@ -1,5 +1,21 @@
+//С одним открытым спойлером
 $(document).ready(function () {
   $('.spoiler__title').click(function (event) {
+    if ($('.spoilers').hasClass('one')) {
+      $('.spoiler__title').not($(this)).removeClass('active');
+      $('.spoiler__text').not($(this).next()).slideUp(300);
+    }
+    $(this).toggleClass('active').next().slideToggle(300);
+  });
+});
+
+//Все спойлеры раскрываются
+$(document).ready(function () {
+  $('.accordion__title').click(function (event) {
+    if ($('.accordion').hasClass('one')) {
+      $('.accordion__title').not($(this)).removeClass('active');
+      $('.accordion__text').not($(this).next()).slideUp(300);
+    }
     $(this).toggleClass('active').next().slideToggle(300);
   });
 });
